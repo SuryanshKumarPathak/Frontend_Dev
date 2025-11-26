57e9571200241ef40487f153da3baaadaf7e2dd0
Suryansh Kumar Pathak <rahulbuxar0001@gmail.com>
2025-11-26 12:31:55 +0530

    Suryansh added js-operator solution

---
+++ Dashboard.js
@@ -0,0 +1,4 @@
+let x = 16.75;
+let summary = `Rounded: ${Math.round(x)} Square Root: ${Math.sqrt(x)} Power (x^3): ${Math.pow(x, 3)} Random (10–50): ${Math.floor(Math.random() * 41) + 10}
+`;
+console.log(summary);
---
+++ DiscountEvaluator.js
@@ -0,0 +1,15 @@
+const cart = [
+    { item:"Litti",category:"food",price:40},
+    { item:"Shoes",category:"fashion",price:2500},
+    { item:"Book",category:"education",price:600}
+];
+let total =0;
+for (let item of cart) {
+    let discount =0;
+    if (item.category=== "electronics")discount = 10;
+    if (item.category=== "fashion")discount =5;
+    let finalPrice=item.price-(item.price*discount/100);
+    total +=finalPrice;
+}
+if (total > 50000) total -= total * 0.05;
+console.log("Final Total:", Math.round(total));
